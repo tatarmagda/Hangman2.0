dc01585d7ff4ce7b1fd3a9a90d6124885c7ecf41
tatarmagda <tatar.magdalena@wp.pl>
2022-02-19 19:45:39 +0100

    change picture

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -16,26 +18,51 @@ class Home extends StatelessWidget {
                 fontSize: 25),
           ),
         ),
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Image.asset("assets/img/hangman.jpg"),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("New Game"),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 75.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Wall of fame"),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                ("assets/img/hangman2.0.jpg"),
               ),
+              fit: BoxFit.fill,
+              colorFilter: ColorFilter.mode(Colors.grey, BlendMode.softLight),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Log out"),
-              style: ElevatedButton.styleFrom(primary: Colors.red[600]),
+          ),
+          child: Container(
+            child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("New Game"),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 20, // Elevation
+                        shadowColor: Colors.black,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 100.0),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Wall of fame"),
+                        style: ElevatedButton.styleFrom(
+                          elevation: 20, // Elevation
+                          shadowColor: Colors.black,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Log out"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red[600], elevation: 20, // Elevation
+                        shadowColor: Colors.red,
+                      ),
+                    ),
+                  ]),
             ),
-          ]),
+          ),
         ));
   }
 }
